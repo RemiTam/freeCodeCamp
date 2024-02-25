fb11b6c0ff32e270fe17047f72368ec87b2a9351
Remi <133393367+RemiTam@users.noreply.github.com>
2024-02-25 18:01:21 +0200

    Update index.js

--- Backend/timestamp-project/index.js
+++ Backend/timestamp-project/index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,30 +21,35 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
 //Timestamp API
 app.get("/api/:date?", (req, res) => {
   let date = req.params.date;
+  let unix;
 
-  if(Number(date)) {
-    const unix = date;
-    date = new Date(Number(date));
-    // const formattedDate = date.toLocaleString("en-US", {
-    //   weekday: "short", // Short day name
-    //   day: "2-digit", // 2-digit day of the month
-    //   month: "short", // Short month name
-    //   year: "numeric", // 4-digit year (e.g., 2015)
-    //   timeZone: "UTC", // Ensure UTC time zone
-    // });
-    res.json({"unix": unix, "utc": date});
-  } 
-  else {
-    const unix = Math.floor(new Date(date).getTime());
-    date = new Date(date);
-    res.json({"unix": unix, "utc": date});
+  //If no date was entered
+  if(date === undefined) {
+    date = new Date();
   }
+
+  //check if unix or date was entered
+  if (Number(date)) {
+    unix = Number(date);
+    date = new Date(Number(date));
+  } else {
+    unix = Math.floor(new Date(date).getTime());
+    date = new Date(date);
+  }
+
+  //if invalid date is entered
+  if (isNaN(date.getTime())) {
+    return res.json({ error: "Invalid Date" })
+  }
+
+  date = date.toUTCString();
+  res.json({"unix": unix, "utc": date})
 })
 
 
